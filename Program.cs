63d382be52a1d36486f2905c5683d7e2592bf5b2
nettnott <nett.work.stud@gmail.com>
2025-09-15 11:25:16 +0300

    menu in a procedure now

--- Program.cs
+++ Program.cs
@@ -3,13 +3,19 @@ int[] cost = [];
 string[] prod = [];
 
 Console.WriteLine("это штука для учета расходов");
-Console.WriteLine("What do u want to do?");
-Console.WriteLine("Press 1 to input data, 2 to see stats, 3 to sort by cost");
-Console.WriteLine("4 to convert money, 5 to search by name, 0 to exit");
-char option = Convert.ToChar(Console.ReadKey());
+void menu()
+{
+    Console.WriteLine("What do u want to do?");
+    Console.WriteLine("Press 1 to input data, 2 to see stats, 3 to sort by cost");
+    Console.WriteLine("4 to convert money, 5 to search by name, 0 to exit");
+    char option = Convert.ToChar(Console.ReadKey());
+};
+menu();
+
 switch (option)
 {
     case '1': //input data
+
 
     case '2': // stats
 
