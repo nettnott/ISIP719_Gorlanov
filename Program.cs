b926a3e87ba2f50465f74c57a427dbddf16a460a
nettnott <nett.work.stud@gmail.com>
2025-09-15 11:22:17 +0300

    добавлено меню без всего

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+int[] cost = [];
+string[] prod = [];
+
+Console.WriteLine("это штука для учета расходов");
+Console.WriteLine("What do u want to do?");
+Console.WriteLine("Press 1 to input data, 2 to see stats, 3 to sort by cost");
+Console.WriteLine("4 to convert money, 5 to search by name, 0 to exit");
+char option = Convert.ToChar(Console.ReadKey());
+switch (option)
+{
+    case '1': //input data
+
+    case '2': // stats
+
+    case '3': // bubble sort
+
+    case '4': // convertation
+
+    case '5': // search by name
+
+    case '0': // exit
+        break;
+}
