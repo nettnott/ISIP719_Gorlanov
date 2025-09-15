75c1ab1a8ae8cb57c0f7e31bb1ec5bf433a674b6
nettnott <nett.work.stud@gmail.com>
2025-09-15 11:40:13 +0300

    menu ne sdelano v procedure.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
-int[] cost = [];
+using static System.Net.Mime.MediaTypeNames;
+
+double[] cost = [];
 string[] prod = [];
 
 Console.WriteLine("это штука для учета расходов");
-char menu()
-{
-    Console.WriteLine("What do u want to do?");
-    Console.WriteLine("Press 1 to input data, 2 to see stats, 3 to sort by cost");
-    Console.WriteLine("4 to convert money, 5 to search by name, 0 to exit");
-    char option = Convert.ToChar(Console.ReadKey());
-    return option;
-};
 
-menu();
+Console.WriteLine("What do u want to do?");
+Console.WriteLine("Press 1 to input data, 2 to see stats, 3 to sort by cost");
+Console.WriteLine("4 to convert money, 5 to search by name, 0 to exit");
+char option = Convert.ToChar(Console.ReadKey());
 
 switch (option)
 {
     case '1': //input data
-        Console.WriteLine("Write ur data in format: 'name'; 'cost'");
-        string tupoyvvod = Console.ReadLine();
-
+        Console.WriteLine("HOW MUCH POSITIONS DO U WANT TO INPUT (from 2 to 40)");
+        int i = Convert.ToInt32(Console.ReadKey());
+        for (int j = 0; j < i; j++)
+        {
+            Console.WriteLine("Write ur data in format: 'name'; 'cost'");
+            string tupoyvvod = Console.ReadLine();
+            string[] idiotizm = tupoyvvod.Split(new char[] { ';' });
+        }
 
     case '2': // stats
 
@@ -32,5 +34,3 @@ switch (option)
     case '0': // exit
         break;
 };
-
-menu();
