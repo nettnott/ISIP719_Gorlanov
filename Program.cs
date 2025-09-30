6669dd54a1363eaaf94a5ddb4430d3b465af24bd
nettnott <nett.work.stud@gmail.com>
2025-09-30 09:53:00 +0300

    ONO NE RABOTAET

--- Program.cs
+++ Program.cs
@@ -1,188 +1,117 @@
 ﻿using System;
-using System.Collections;
-using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
-Console.WriteLine("это штука для учета расходов");
-
-Console.WriteLine("HOW MUCH POSITIONS DO U WANT TO INPUT (from 2 to 40)");
-int n = Convert.ToInt32(Console.ReadLine());
-
-double[] cost = new double [n];
-string[] prod = new string [n];
-
-for (int i = 0; i < n; i++)
+class TextStatistics
 {
+    public string ShortestWord;
+    public string LongestWord;
+    public int WordCount;
+    public int SentenceCount;
+    public int VowelCount;
+    public int ConsonantCount;
+    public Dictionary<char, int> LetterFrequency = new Dictionary<char, int>();
 
-    Console.WriteLine("Write ur data in format: 'name';'cost'");
-    string tupoyvvod = Console.ReadLine();
-    string[] idiotizm = tupoyvvod.Split(';');
-    prod[i] = idiotizm[0].Trim();
-    cost[i] = double.Parse(idiotizm[1].Trim());
-};
-
-double summa(double[] cost)
-{
-    double sum = 0;
-    foreach (double c in cost)
+    public void Print()
     {
-        sum += c;
-    };
-    return sum;
-};
+        Console.WriteLine("STatistika teksta");
+        Console.WriteLine($"Words: {WordCount}");
+        Console.WriteLine($"Sentences: {SentenceCount}");
+        Console.WriteLine($"Thhe most korotkoe word: {ShortestWord}");
+        Console.WriteLine($"The most dlinnoe word: {LongestWord}");
+        Console.WriteLine($"Glasnih: {VowelCount}");
+        Console.WriteLine($"Soglasnih: {ConsonantCount}");
+        Console.WriteLine("Frequency bukw:");
+        foreach (var pair in LetterFrequency)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+    }
+}
 
-void menu()
+class Program
 {
-    Console.WriteLine();
-    Console.WriteLine("What do u want to do?");
-    Console.WriteLine("Press 1 to see your data, 2 to see stats, 3 to sort by cost");
-    Console.WriteLine("4 to convert money, 5 to search by name, 0 to exit");
-    int option = Convert.ToInt32(Console.ReadLine());
-
-    switch (option)
+    // if glasnyaya
+    static bool IsVowel(char c)
     {
-        case 1: //see 
-            Console.WriteLine();
-            for (int s = 0; s < prod.Length; s++)
+        char lower = Char.ToLower(c);
+        return "аеёиоуыэюя".IndexOf(lower) >= 0;
+    }
+
+    static TextStatistics AnalyzeText(string text)
+    {
+        TextStatistics stats = new TextStatistics();
+
+        string[] words = text.Split(new char[] { ' ', ',', '.', '!', '?', ';', ':', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        stats.WordCount = words.Length;
+
+        // the most korotkoe and dlinnoe
+        stats.ShortestWord = words[0];
+        stats.LongestWord = words[0];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string w = words[i];
+            if (w.Length < stats.ShortestWord.Length) stats.ShortestWord = w;
+            if (w.Length > stats.LongestWord.Length) stats.LongestWord = w;
+        }
+
+        // Sentences
+        string[] sentences = text.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        stats.SentenceCount = sentences.Length;
+
+        // Bukws count and stats
+        foreach (char c in text)
+        {
+            if (Char.IsLetter(c))
             {
-                Console.WriteLine($"{prod[s]} {cost[s]}");
+                char lower = Char.ToLower(c);
+
+                if (IsVowel(lower)) stats.VowelCount++;
+                else stats.ConsonantCount++;
+
+                if (!stats.LetterFrequency.ContainsKey(lower))
+                    stats.LetterFrequency[lower] = 0;
+                stats.LetterFrequency[lower]++;
             }
-            break;
+        }
 
-        case 2: // stats
+        return stats;
+    }
 
-            Console.WriteLine("What do u want to see?");
-            Console.WriteLine("Press 1 to see average, 2 to see max, 3 to see min, 4 to see sum");
-            int stat = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-            switch (stat)
-            {
-                case 1: // average
+    static void Main()
+    {
+        PublicKey static void
+        List<TextStatistics> history = new List<TextStatistics>();
+        Console.WriteLine("if u want to do that shit print 1, if u want to see history print 2, either print 0");
+        string i = Console.ReadLine();
+        switch (i)
+        {
+            case "1":
+                Console.WriteLine("Input text (at least100 simvolow:");
+                string input = Console.ReadLine();
 
-                    double avg = summa(cost) / cost.Length;
-                    Console.WriteLine($"average = {avg}");
-                    break;
-
-                case 2: // max
-
-                    double maxon = 0;
-                    foreach (double i in cost)
+                if (input.Length < 100)
                     {
-                        if (i > maxon)
-                        {
-                            maxon = i;
-                        }
-                    };
-                    Console.WriteLine($"max = {maxon}");
-                    break; 
-
-                case 3: // min
-
-                    double minipig = 99999999999;
-                    foreach (double i in cost)
-                    {
-                        if (i < minipig)
-                        {
-                            minipig = i;
-                        }
-                    };
-                    Console.WriteLine($"min = {minipig}");
-                    break;
-
-                case 4: // summa
-
-                    Console.WriteLine($"summa = {summa(cost)}");
-                    break;
-            };
-            break;
-
-        case 3: // bubble sort
-
-            for (int j = 0; j <= cost.Length - 2; j++)
-            {
-                for (int i = 0; i <= cost.Length - 2; i++)
-                {
-                    if (cost[i] > cost[i + 1])
-                    {
-                        double temp = cost[i + 1];
-                        string tmp = prod[i+1];
-                        cost[i + 1] = cost[i];
-                        prod[i+1] = prod[i];
-                        cost[i] = temp;
-                        prod[i] = tmp;
-                    }
+                        Console.WriteLine("Dlinnee nado");
                 }
-            }
-            for (int s = 0; s < prod.Length; s++)
-            {
-                Console.WriteLine($"{prod[s]} {cost[s]}");
-            };
-            break;
 
-        case 4: // convertation
+                TextStatistics result = AnalyzeText(input);
+                history.Add(result);
 
-            Console.WriteLine();
-            Console.WriteLine("How do you want to convert?");
-            Console.WriteLine("Press 1 to input ur course (rub to smth), 2 to choose from the list");
-            int crsopt = Convert.ToInt32(Console.ReadLine());
-            switch (crsopt)
-            {
-                case 1:
-                    Console.WriteLine("write your course");
-                    double course = Convert.ToDouble(Console.ReadLine());
-                    for (int s = 0; s < prod.Length; s++)
-                    {
-                        Console.WriteLine($"{prod[s]} {cost[s] / course}");
-                    };
-                    break ;
-                case 2:
-                    Console.WriteLine("Choose a course");
-                    Console.WriteLine("1 - rub to usd: 90,25; 2 - rub to eur: 98,50; 3 - rub to jpy: 0,59");
-                    int val = Convert.ToInt32(Console.ReadLine());
-                    switch (val)
-                    {
-                        case 1: //to usd
-                            Console.WriteLine("rub to usd");
-                            for (int s = 0; s < prod.Length; s++)
-                            {
-                                Console.WriteLine($"{prod[s]} {cost[s] / 90,25}");
-                            };
-                            break;
-                        case 2: // to eur
-                            Console.WriteLine("rub to eur");
-                            for (int s = 0; s < prod.Length; s++)
-                            {
-                                Console.WriteLine($"{prod[s]} {cost[s] / 98,50}");
-                            };
-                            break;
-                        case 3: // to jpy
-                            Console.WriteLine("rub to jpy");
-                            for (int s = 0; s < prod.Length; s++)
-                            {
-                                Console.WriteLine($"{prod[s]} {cost[s] / 0,59}");
-                            };
-                            break;
-                    }
-                    break ;
-            }
-            break;
-
-        case 5: // search by name
-
-            Console.WriteLine("Vvedite nachalo stroki");
-            string search = Console.ReadLine();
-            for (int i = 0; i < prod.Length; i++)
-            {
-                bool result = prod[i].ToLower().Contains(search.ToLower());
-                if (result == true)
+                result.Print();
+                break;
+            case "2":
+                foreach(TextStatistics g in  history)
                 {
-                    Console.WriteLine($"{prod[i]} {cost[i]}");
+                    Console.WriteLine(g);
                 }
-            };
-            break;
-
-        case 0: // exit
-            return;
-    };
-    menu();
-};
-menu();
+                break;
+            case "0":
+                break;
+        }
+        Main();
+        
+    }
+}
