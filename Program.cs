93fde8d679f79da7a5b120f44a92c693d449d143
nettnott <nett.work.stud@gmail.com>
2025-10-14 11:41:44 +0300

    dobavleni predmeti: equipment and weapon

--- Program.cs
+++ Program.cs
@@ -3,6 +3,39 @@
 public class gaymer
 {
     public int hp = 100;
+    public bool debuff = false;
+    public int def = 0;
+    public int atk = 5;
+    public EquipTypes equip;
+    public WeaponTypes weapon;
+
+    public enum WeaponTypes { palka, ak, oreshnik};
+    if (weapon == WeaponTypes.palka) 
+        { 
+            atk += 10;
+        }
+    else if (weapon == WeaponTypes.ak)
+        { 
+            atk += 30;
+        }
+    else if (weapon == WeaponTypes.oreshnik)
+        { 
+            atk += 40;
+        }
+
+public enum EquipTypes { perchatki, podkraduli, bdsmcostume };
+if (equip == EquipTypes.perchatki)
+{
+    def += 10;
+}
+else if (equip == EquipTypes.podkraduli)
+{
+    def += 30;
+}
+else if (equip == EquipTypes.bdsmcostume)
+{
+    def += 40;
+}
 
 }
 
@@ -15,7 +48,18 @@ public class enemy
 
 public class goblin : enemy
 {
+    //имеет шанс нанести критический урон
 
+}
+
+public class skelet : enemy
+{
+    //игнорирует защиту игрока.
+}
+
+public class mag : enemy
+{
+    //имеет шанс наложить «заморозку» (игрок пропускает следующий ход).
 }
 
 public class items
