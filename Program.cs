29c126bff64d1d19227aab828cfdc149c612bee6
nettnott <nett.work.stud@gmail.com>
2025-10-20 09:39:21 +0300

    dobavleni predmeti

--- Program.cs
+++ Program.cs
@@ -1,86 +1,110 @@
 ﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 
-public class gaymer
+class Weapon
 {
-    public int hp = 100;
+    public  Random Rand = new Random();
+    int atk { get; set; }
+    string name { get; set; }
+    string[] strings = { "Cocherga deada", "Oreshnik", "Spidoznaya igolka", "Otcislenie" };
+    public Weapon()
+    {
+        name = strings[Rand.Next(0,4)];
+        atk = Convert.ToInt32(Rand.Next(3, 75));
+    }
+}
+class equipment
+{
+    public Random Rand = new Random();
+    int def { get; set; }
+    string name { get; set; }
+    string[] strings = { "Trusi deada", "Lapti", "Futbolka 'lubluy emo'", "Kstum s pohoron" };
+    public equipment()
+    {
+        name = strings[Rand.Next(0, 4)];
+        def = Convert.ToInt32(Rand.Next(3, 75));
+    }
+}
+
+public class Player
+{
+    public int hp ;
     public bool debuff = false;
-    public int def = 0;
-    public int atk = 5;
-    public EquipTypes equip;
-    public WeaponTypes weapon;
-
-    public enum WeaponTypes { palka, ak, oreshnik};
-    if (weapon == WeaponTypes.palka) 
-        { 
-            atk += 10;
-        }
-    else if (weapon == WeaponTypes.ak)
-        { 
-            atk += 30;
-        }
-    else if (weapon == WeaponTypes.oreshnik)
-        { 
-            atk += 40;
-        }
-
-public enum EquipTypes { perchatki, podkraduli, bdsmcostume };
-if (equip == EquipTypes.perchatki)
-{
-    def += 10;
+    public int def ;
+    public int atk ;
+    Weapon Weapon { get; set; }
+    equipment equipment { get; set; }
+    public Player(int hp, int def, int atk)
+    {
+        this.hp = hp;
+        this.def = def;
+        this.atk = atk;
+    }
 }
-else if (equip == EquipTypes.podkraduli)
-{
-    def += 30;
-}
-else if (equip == EquipTypes.bdsmcostume)
-{
-    def += 40;
-}
+/* подо мной м5 асфальт 8 у нее биркин цвета осень она закурит но я бросил*/
 
-}
-
-public class enemy
+public class Enemy
 {
-    public int hp = 100;
+    public int hp;
     public int atk;
-    public int def = 100;
-}
-
-public class goblin : enemy
-{
-    //имеет шанс нанести критический урон
-
-}
-
-public class skelet : enemy
-{
-    //игнорирует защиту игрока.
-}
-
-public class mag : enemy
-{
-    //имеет шанс наложить «заморозку» (игрок пропускает следующий ход).
-}
-
-public class items
-{
-    public enum { zelie, weapon, dospeh };
-
-    public void take() { };
-
-    public void tossaway() { };
-
-    public void chest()
+    public int def;
+    public Enemy(int hp, int def, int atk)
+    {
+        this.hp = hp;
+        this.def = def;
+        this.atk = atk;
+    }
+    public virtual void attac()
     {
 
     }
 }
 
+public class Goblin : Enemy
+{
+    //имеет шанс нанести критический урон
+    public Goblin(int hp, int def, int atk) :base(hp, def, atk)
+    {
+
+    }
+    public override void attac()
+    {
+        base.attac();
+
+    }
+}
+
+public class Skelet : Enemy
+{
+    //игнорирует защиту игрока.
+}
+
+public class Mag : Enemy
+{
+    //имеет шанс наложить «заморозку» (игрок пропускает следующий ход).
+}
+
+//public class Items
+//{
+//    public void take() { }
+
+//    public void tossaway() { }
+
+//    public void chest()
+//    {
+
+//    }
+//}
+
 public class gaym
 {
-    public void battle() 
-    { 
-    
-    };
+    int hod;
+    public  Player player = new Player(100, 1, 5);
+    public void start() {
+        do
+        {
 
+        } while (player.hp <= 0); 
+    }
 }
